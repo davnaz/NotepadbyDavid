21e7c0efb23b1821128f6019ea9db4e5e7590942
Назлуян Давид <davnazzz@gmail.com>
2018-06-17 21:53:02 +0300

    Feature1 integrated

--- NotepadbyDavid/MainWindow.xaml.cs
+++ NotepadbyDavid/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,26 @@ namespace NotepadbyDavid
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_saveFile_Click(object sender, RoutedEventArgs e)
+        {
+            RichTextBox docBox = this.textEditor;
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text Files (*.txt)|*.txt|RichText Files (*.rtf)|*.rtf|XAML Files (*.xaml)|*.xaml|All files (*.*)|*.*";
+            if (sfd.ShowDialog() == true)
+            {
+                TextRange doc = new TextRange(docBox.Document.ContentStart, docBox.Document.ContentEnd);
+                using (FileStream fs = File.Create(sfd.FileName))
+                {
+                    if (System.IO.Path.GetExtension(sfd.FileName).ToLower() == ".rtf")
+                        doc.Save(fs, DataFormats.Rtf);
+                    else if (System.IO.Path.GetExtension(sfd.FileName).ToLower() == ".txt")
+                        doc.Save(fs, DataFormats.Text);
+                    else
+                        doc.Save(fs, DataFormats.Xaml);
+                }
+            }
         }
     }
 }
